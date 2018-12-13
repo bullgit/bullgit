a863a6db4ed044228faa7574d123ac80b5690041
Gregor Adams <greg@pixelass.com>
2018-12-13 22:37:29 +0100

    fix: fixes build
    adds optimization (chunks)

--- config/webpack/config/production.js
+++ config/webpack/config/production.js
@@ -4,7 +4,30 @@ const common = require("./common.js");
 module.exports = async (env, argv) => {
 	return merge(await common(env, argv), {
 		mode: "production",
-		devtool: false
+		devtool: false,
+		optimization: {
+			splitChunks: {
+				chunks: "all",
+				minSize: 30000,
+				maxSize: 300000,
+				minChunks: 1,
+				maxAsyncRequests: 5,
+				maxInitialRequests: 3,
+				automaticNameDelimiter: "~",
+				name: true,
+				cacheGroups: {
+					vendors: {
+						test: /[\\/]node_modules[\\/]/,
+						priority: -10
+					},
+					default: {
+						minChunks: 2,
+						priority: -20,
+						reuseExistingChunk: true
+					}
+				}
+			}
+		},
 	});
 };
 
